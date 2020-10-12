19035a671a9f6155472b7fdb9bcf28357ccd4f7d
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2020-10-12 15:06:17 +0300

    ADDDIN PAGES? THIRD LESSON
    Took 50 minutes

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'firstapp/post_list.html', {})
+
+
+def about_page(requst):
+    return render(requst, 'firstapp/about.html')
