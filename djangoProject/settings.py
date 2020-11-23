87bd50eacbfedcc6baee2b8c12d5857f8d8f848d
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-23 07:19:21 +0300

    delete: techPractice4term app
    Took 6 minutes

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'techPractice4term.apps.Techpractice4TermConfig',
     'firstapp'
 ]
 
--- techPractice4term/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Techpractice4TermConfig(AppConfig):
-    name = 'techPractice4term'
