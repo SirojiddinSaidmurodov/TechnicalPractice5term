4761488da6e1d65f874ed32e2028c13698c13e9b
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-29 20:10:43 +0300

    edit: added posting
    Took 1 hour 0 minutes

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('about', views.about_page, name='about_page'),
     path('list/', views.index, name='index_list'),
+    path('form/', views.post, name='post'),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from firstapp.forms import PostForm
 from firstapp.models import Post
 
 
@@ -15,3 +16,15 @@ def about_page(request):
 def index(request):
     text = Post.objects.all()
     return render(request, "firstapp/index.html", {"text": text})
+
+
+def post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'firstapp/post_form.html', {'form': form})
